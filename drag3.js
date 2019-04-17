a0c448854d0a7e1559d59ce19ef7228ca82f51a0
pingshim <shiyongchang.love@163.com>
2019-04-17 10:33:12 +0800

    changed drag3.js by new1

--- drag3.js
+++ drag3.js
@@ -2,7 +2,14 @@ function drag(id){
     var obj = document.getElementById(id);
     var disX = 0;
     var disY = 0;
-	obj.onmousedown = function(){
-	var a = 0;    	
+	obj.onmousedown = function(ev){
+	disX = ev.pageX - obj.offsetLeft;
+	disY = ev.pageY - obj.offsetTop;
+	
+	document.onmousemove = function(ev){
+		obj.style.left = ev.clientX - disX + "px";
+		obj.style.top = ev.clientY - disX + "px";
+	}
+		
     }
 }
