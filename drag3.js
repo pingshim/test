70fa2c9da75852a9b29f0f4da76a72db05ab748b
pingshim <shiyongchang.love@163.com>
2019-04-17 10:23:40 +0800

    changed the drag3.js from master

--- drag3.js
+++ drag3.js
@@ -2,7 +2,12 @@ function drag(id){
     var obj = document.getElementById(id);
     var disX = 0;
     var disY = 0;
-	obj.onmousedown = function(){
-	var a = 0;    	
+	obj.onmousedown = function(ev){
+	disX = ev.pageX - obj.offsetLeft;
+	disY = ev.pageY - obj.offsetTop;
+	document.onmousemove = function(ev){
+		obj.style.left = ev.pageX - disX + 'px';
+		obj.style.top = ev.pageY - disY + 'px';
+	}
     }
 }
